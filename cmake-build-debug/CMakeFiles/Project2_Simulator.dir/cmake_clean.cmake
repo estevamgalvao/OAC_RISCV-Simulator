a5faef0a354b309145cf0d00854aa26097bb62f4
Estevam Albuquerque <estevamgalvao@gmail.com>
2019-04-20 23:19:49 -0300

    start

---
+++ cmake-build-debug/CMakeFiles/Project2_Simulator.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Project2_Simulator.dir/main.c.o"
+  "Project2_Simulator.pdb"
+  "Project2_Simulator"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Project2_Simulator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
